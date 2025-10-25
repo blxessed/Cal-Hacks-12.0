266907d99c950a2d7e4df55e8018473a09aafc44
BLXESSED <78892114+BLXESSED@users.noreply.github.com>
2025-10-25 13:36:49 -0700

    Update index.js

--- private/index.js
+++ private/index.js
@@ -1,2 +1,25 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+const axios = require('axios');
 
+const websiteScrape = async (url) => {
+  try {
+    let scrapeResults;
+
+    const response = await axios.get(url, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+      },
+      timeout: 15000
+    });
+    const html = response.data; 
+
+    scrapeResults = scrapeResults
+
+    console.log("Scraped:", url );
+    return JSON.stringify({ results: scrapeResults });
+
+  } catch (error) {
+    console.error("General error:", error.response?.data || error.message);
+    return JSON.stringify({ error: "General error, try again later" });
+  }
+};
